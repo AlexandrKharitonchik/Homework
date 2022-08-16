4cb64f3d14d5ff6d59ee9104a22b523c88eef65d
AlexandrKharitonchik <demoris@tut.by>
2022-08-16 21:31:56 +0300

    up

--- src/home_work_2/arrays/IArraysOperation.java
+++
@@ -1,7 +0,0 @@
-package home_work_2.arrays;
-
-public interface IArraysOperation {
-    void arrToConsole(int[] array); // Методы, которые будут дописывать далее в реализующих интерфейс объектах
-    void everySecondElem(int[] array);
-    void reversedElems(int[] array);
-}
